6a0ff07122f6cbf86354bcb07938ef02cb599e74
HexodusGym <hexodusfitnesscenter@gmail.com>
2026-03-14 06:38:39 -0600

    Delete components directory

--- components/caja/index.ts
+++
@@ -1,5 +0,0 @@
-// Componentes de gestión de caja
-export { IndicadorCaja } from "./indicador-caja"
-export { ModalAperturaCaja } from "./modal-apertura-caja"
-export { ModalCierreCaja } from "./modal-cierre-caja"
-export { CajaGuard } from "./caja-guard"
